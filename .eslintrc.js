abb908a17e0d24c11aba9cd354944879e7bfdaf0
carlba <genzorg@gmail.com>
2022-01-02 11:38:57 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      1,
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'max-classes-per-file': 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+  },
+};
